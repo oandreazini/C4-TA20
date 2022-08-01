177f7843d4a31dd0d275a3fc6587235b233987a3
oandreazini <oriol_as_92@hotmail.com>
2022-08-01 15:52:39 +0200

    Ex8 Finished v1.1

---
+++ UD20_08/src/test/java/UD20_08/UD20_08/AppTest.java
@@ -0,0 +1,20 @@
+package UD20_08.UD20_08;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
