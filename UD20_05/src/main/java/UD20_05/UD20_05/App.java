68d81c23429af65d00f7f4d44c1b598a29741bb4
oandreazini <oriol_as_92@hotmail.com>
2022-08-01 11:58:38 +0200

    Estructura Ex5

---
+++ UD20_05/src/main/java/UD20_05/UD20_05/App.java
@@ -0,0 +1,13 @@
+package UD20_05.UD20_05;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ UD20_05/src/main/java/UD20_05/UD20_05/GraphicApp5.java
@@ -0,0 +1,5 @@
+package UD20_05.UD20_05;
+
+public class GraphicApp5 {
+
+}
