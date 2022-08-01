fd56a28bdaea91303408f8e50f881c75fcbc908f
rrrcoba <roger-decroly@hotmail.com>
2022-08-01 10:14:30 +0200

    added exercise 3 project structure

---
+++ UD20_03/src/main/java/UD20_03/UD20_03/App.java
@@ -0,0 +1,13 @@
+package UD20_03.UD20_03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
