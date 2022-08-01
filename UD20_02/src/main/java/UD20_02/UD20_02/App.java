09a66e1892a639916d78f26756a25a55960fb29e
oandreazini <oriol_as_92@hotmail.com>
2022-08-01 09:35:34 +0200

    Initial Ex2

---
+++ UD20_02/src/main/java/UD20_02/UD20_02/App.java
@@ -0,0 +1,13 @@
+package UD20_02.UD20_02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
