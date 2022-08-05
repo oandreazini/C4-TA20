eeba02f60898938f4f70e6748a75cd8131618ded
oandreazini <oriol_as_92@hotmail.com>
2022-08-05 19:18:53 +0200

    Memory Images

--- memory/src/main/java/UD20_9/memory/Controller.java
+++ memory/src/main/java/UD20_9/memory/Controller.java
@@ -9,21 +9,23 @@ import javax.swing.JPanel;
 public class Controller {
 	
 
-	private static final int N_BUTTONS = 16; //constantes de la cantidad de botones
-	
-	private DashBoard window;
-	
+	private static final int GAMEBORAD = 16; //constantes de la cantidad de botones
 	private Vector<String>number;
+	private DashBoard[] buttons;
 	private Random rand;
 	private int rad;
 	private int token;
+	private int shift;
+	private int type1;
+	private int type;
+	private int indenti1;
+	private int identi2;
+	private int count;
 	
 	// Se inicializan los botones y variables
-	public Controller() throws IOException{
-		
-		window = new DashBoard();
-		
-		token = N_BUTTONS ;
+	public Controller(){
+		token = GAMEBORAD ;
+		buttons = new DashBoard[GAMEBORAD];
 		number = new Vector<String>();
 		rand = new Random();
 		makeRandom();
@@ -31,7 +33,7 @@ public class Controller {
 	
 	// LLena el vector "number" con parejas del 1 al 8
 	public void makeRandom() {
-		for(int i=0; i<8; i++) {
+		for(int i=0; i<(GAMEBORAD/2); i++) {
 			number.addElement(String.valueOf(i+1));
 			number.addElement(String.valueOf(i+1));
 		}
@@ -45,5 +47,14 @@ public class Controller {
 		number.removeElementAt(rad);
 		token-=1;
 		return back;
+	}
+	
+	// Crea los botones, les asigna el numero random y una id
+	public DashBoard[] createButtons() {
+		for(int i=0; i<GAMEBORAD; i++) {
+			buttons[i] = new DashBoard();
+			buttons[i].setNumero(randomNumber());
+			buttons[]
+		}
 	}
 }
