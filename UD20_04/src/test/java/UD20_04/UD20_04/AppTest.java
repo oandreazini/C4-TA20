862b548ea7a60ba7dc24b3710dcfb950f0c97000
oandreazini <oriol_as_92@hotmail.com>
2022-08-05 14:36:39 +0200

    Create Ex4

---
+++ UD20_04/src/test/java/UD20_04/UD20_04/AppTest.java
@@ -0,0 +1,20 @@
+package UD20_04.UD20_04;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
